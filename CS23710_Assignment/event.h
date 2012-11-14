e14088cf543bc43dc733b6bf746ea202a247c099
Chris Savill <cls102@CLS-ThinkPad.(none)>
2012-11-14 12:03:57 +0000

    CS23710_Assignment Added to Repo

---
+++ CS23710_Assignment/event.h
@@ -0,0 +1,22 @@
+/* 
+ * File:   event.h
+ * Author: Chris Savill - chs17
+ * Description: File that defines the structures used in the event monitoring program
+ */
+
+#ifndef EVENT_H
+#define	EVENT_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* EVENT_H */
+
---
+++ CS23710_Assignment/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: cls102
+ *
+ * Created on 14 November 2012, 11:46
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
